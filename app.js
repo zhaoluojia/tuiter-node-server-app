1077e64b64cf474ff6b3debeee26ad995e9d8257
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-11-15 22:35:00 -0800

    start a9

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
@@ -11,3 +12,6 @@ TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
